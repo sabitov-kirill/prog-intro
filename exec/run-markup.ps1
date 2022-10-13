03a1bfdec3efbfe32badad35b3ec136468ca7efd
sabitov-kirill <sabitovkirill@ya.ru>
2022-10-13 10:18:12 +0300

    Deleted poweshell script.

--- exec/run-markup.ps1
+++
@@ -1,5 +0,0 @@
-Copy-Item ./../../prog-intro-tests/java/markup/*.java ./../java-solutions/markup/
-javac -sourcepath ../java-solutions/ -d ../out ../java-solutions/markup/*.java
-javac -encoding utf8 -sourcepath ./../../prog-intro-tests/java/ -d ../out ./../../prog-intro-tests/java/markup/*.java
-cd ../out
-java -ea markup.MarkupTest Base
