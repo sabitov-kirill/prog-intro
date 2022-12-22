7f221a427a51a52c575c656908f6ad5b64e6b547
sabitov-kirill <sabitovkirill@ya.ru>
2022-12-22 21:24:55 +0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit e36a7708ce4d2aa43b7cda018da1a8651f3691db.

--- java-solutions/expression/exceptions/ExpressionParser.java
+++ java-solutions/expression/exceptions/ExpressionParser.java
@@ -1,13 +1,15 @@
 package expression.exceptions;
 
 import expression.*;
-import expression.parser.*;
+import expression.parser.ParserArgumentExpectedException;
+import expression.parser.ParserEOFException;
+import expression.parser.ExpressionParserImpl;
+import expression.parser.ParserUnexpectedCharException;
 
 public class ExpressionParser implements TripleParser {
     @Override
     public TripleExpression parse(String expression)
-            throws ParserArgumentExpectedException, ParserEOFException,
-            ParserUnexpectedCharException, ParserConstantOverflowException {
+            throws ParserArgumentExpectedException, ParserEOFException, ParserUnexpectedCharException {
         ExpressionParserImpl parser = new ExpressionParserImpl(expression, new CheckedOperatorFactory());
         return parser.parse();
     }
